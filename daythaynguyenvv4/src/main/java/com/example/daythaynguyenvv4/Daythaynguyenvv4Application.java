de89bd8c0f7e8d466c582d26bf8f5aafa0e9a7ac
hanq11 <hanq11@fpt.edu.vn>
2025-01-13 16:52:51 +0700

    udpate

---
+++ daythaynguyenvv4/src/main/java/com/example/daythaynguyenvv4/Daythaynguyenvv4Application.java
@@ -0,0 +1,13 @@
+package com.example.daythaynguyenvv4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Daythaynguyenvv4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Daythaynguyenvv4Application.class, args);
+	}
+
+}
---
+++ daythaynguyenvv4/src/test/java/com/example/daythaynguyenvv4/Daythaynguyenvv4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daythaynguyenvv4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Daythaynguyenvv4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
