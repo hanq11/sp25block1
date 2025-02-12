c07ed0ca2c6e7b75a82285d7bf4865c2e26e4bdf
hanq11 <hanq11@fpt.edu.vn>
2025-02-12 17:52:57 +0700

    update

---
+++ SD19302SOF3022/src/main/java/com/example/sd19302sof3022/Sd19302sof3022Application.java
@@ -0,0 +1,13 @@
+package com.example.sd19302sof3022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sd19302sof3022Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sd19302sof3022Application.class, args);
+    }
+
+}
